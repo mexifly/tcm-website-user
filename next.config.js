4bb2b183ad8e8c21b4b0273fbd57c3677af1e688
ZackHu26 <57276154+ZackHu26@users.noreply.github.com>
2023-12-08 16:25:07 -0800

    Update next.config.js with new db

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   env: {
         'MYSQL_HOST': '127.0.0.1',
         'MYSQL_PORT': '3306',
-        'MYSQL_DATABASE': 'test',
+        'MYSQL_DATABASE': 'admin',
         'MYSQL_USER': 'root',
         'MYSQL_PASSWORD': '123456',
         'MY_API_KEY': 'Bearer AtNcesaNx5nkRKjUdwEI5MZM7XikjExB4twJoxJi'
